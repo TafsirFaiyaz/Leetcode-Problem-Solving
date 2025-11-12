db7b1c07ea560d569b7d53ff95266d3f922f3076
Tafsir Faiyaz <tafsirahmedfaiyaz@gmail.com>
2025-11-12 21:07:32 +0600

    Added Prob-69 which is Sqrt(x) without builtin function or operator

---
+++ Prob-69.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def mySqrt(self, x):
+
+        if x < 2:
+            return x
+
+        left, right = 1, x//2
+
+        while left<=right:
+            mid = (left+right)//2
+            z = mid * mid
+
+            if z == x:
+                return mid
+
+            elif z < x:
+                left = mid + 1
+
+            else:
+                right = mid-1
+
+        return right
+
+        
