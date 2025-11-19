9ddbbff87868f140900b15142192a9610f4cb0b3
Tafsir Faiyaz <tafsirahmedfaiyaz@gmail.com>
2025-11-19 22:04:42 +0600

    Solved Prob-125 which is Valid Palindrome

---
+++ Prob-125.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        import re
+        text = s.lower()
+        result = re.sub(r'[^A-Za-z0-9]', '', text)
+        n = len(result)
+        for i in range(n//2):
+            if result[i] != result[n-1-i]:
+                return False
+        return True
