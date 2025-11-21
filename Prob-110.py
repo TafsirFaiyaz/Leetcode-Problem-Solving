429754cb0af80126c48c42e817ae6d036835e276
Tafsir Faiyaz <tafsirahmedfaiyaz@gmail.com>
2025-11-22 03:56:22 +0600

    Solved Prob-110 Balanced Binary Tree

---
+++ Prob-110.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isBalanced(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: bool
+        """
+        def height(node):
+            if not node:
+                return 0
+
+            left = height(node.left)
+            right = height(node.right)
+
+            if left == -1 or right == -1 or abs(left - right) > 1:
+                return -1
+
+            return 1 + max(left, right)
+
+        return height(root) != -1
--- Prob-118.py
+++ Prob-118.py
@@ -4,17 +4,17 @@ class Solution(object):
         :type numRows: int
         :rtype: List[List[int]]
         """
-        if numRows<2:
+        if numRows<2:               # edge case
             return [[1]]
 
         res = [[1]]
         for i in range(2,numRows+1):
             l = []
             for j in range(i):
-                if j == 0 or j == i - 1:
+                if j == 0 or j == i - 1:  # first and last element of each row is 1
                     l.append(1)
                 else:
-                    l.append(res[-1][j-1] + res[-1][j])
+                    l.append(res[-1][j-1] + res[-1][j])  # adding the two numbers from the previous row
 
             res.append(l)
 
