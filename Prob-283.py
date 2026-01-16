dcb6dc8be3eacf541760951012df03c030112648
Tafsir Faiyaz <tafsirahmedfaiyaz@gmail.com>
2026-01-16 07:15:40 +0600

    Added explanations in 283

--- Prob-283.py
+++ Prob-283.py
@@ -7,10 +7,11 @@ class Solution(object):
 
         pos = 0 
 
-        for i in range(len(nums)):
-            if nums[i] != 0:
-                nums[pos] = nums[i]
+        for i in range(len(nums)):              
+            if nums[i] != 0:                    # If the current element is non-zero
+                nums[pos] = nums[i]             # Move it to the 'pos' index
                 pos += 1
-
+                                                # What happened in the first loop is that all non-zero elements are moved to the front of the array in their original order.
+                                                # Now, we need to fill the rest of the array with zeros.
         for i in range(pos, len(nums)):
             nums[i] = 0
